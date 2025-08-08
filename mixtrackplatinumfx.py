bf3d9cca487924001911e9789e8c062eb38e7dec
Benjamin Ross <bro82@uclive.ac.nz>
2025-08-08 15:40:30 +1200

    Add demo mode script

---
+++ mixtrackplatinumfx.py
@@ -0,0 +1,21 @@
+import rtmidi
+import time
+
+midiout = rtmidi.MidiOut()
+
+available_ports = midiout.get_ports()
+
+if "MixTrack Platinum FX" in available_ports:
+    midiout.open_port(available_ports.index("MixTrack Platinum FX"))
+else:
+    print(f"Port 'MixTrack Platinum FX' not found.")
+    exit(1)
+
+midiout.send_message([0xf0, 0x00, 0x20, 0x7f, 0x03, 0x01, 0xf7])
+midiout.send_message([0xf0, 0x00, 0x20, 0x7f, 0x04, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xf7]) 
+midiout.send_message([0xf0, 0x7e, 0x00, 0x06, 0x01, 0xf7])
+
+
+
+
+
